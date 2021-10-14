0316185369bdfa40d6aa5feb4bb1aa571b054aec
khaldun_askara <mkurotsuchi0@gmail.com>
2021-10-14 13:20:40 +0500

    all is ok

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace knapsack_problem
@@ -23,24 +24,55 @@ namespace knapsack_problem
             Console.WriteLine("");
         }
 
-        static void GrayCode(int N)
+        static int MaskCalc(int N, bool[] mask, int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < N; i++)
+                result += mask[i] ? array[i] : 0;
+            return result;
+        }
+
+        static bool[] GrayCode(int N, int M, int[] mass, int[] costs)
         {
             bool[] S = new bool[N];
             int[] b = new int[N + 1];
             for (int i = 0; i < N + 1; i++)
                 b[i] = i + 1;
             int x = 0;
+
+            int max_cost = 0;
+            bool[] max_knapsack = new bool[N];
+
             do
             {
-                DoSomething(S);
+                // Console.Write("S: ");
+                // DoSomething(S);
+
+                int current_mass = MaskCalc(N, S, mass);
+                int current_cost = MaskCalc(N, S, costs);
+                // Console.WriteLine(current_mass + " " + current_cost);
+                if (current_mass <= M && current_cost > max_cost)
+                {
+                    max_cost = current_cost;
+                    for (int i = 0; i < N; i++)
+                        max_knapsack[i] = S[i];
+                    // Console.Write(max_cost + "\nmax:");
+                    // DoSomething(max_knapsack);
+                }
+
                 x = b[0];
-                b[0] = 1;
-                b[x - 1] = b[x];
-                b[x] = x + 1;
                 if (x <= N)
+                {
+                    b[0] = 1;
+                    b[x - 1] = b[x];
+                    b[x] = x + 1;
                     S[x - 1] = !S[x - 1];
+                }
             }
             while (x <= N);
+            // Console.Write("result: ");
+            // DoSomething(max_knapsack);
+            return max_knapsack;
         }
 
 
@@ -49,7 +81,11 @@ namespace knapsack_problem
         {
             var (mass, costs, N, M) = Input("input.txt");
 
-            GrayCode(N);
+            var result = GrayCode(N, M, mass, costs);
+            // DoSomething(result);
+            for (int i = 0; i < N; i++)
+                Console.Write(result[i] ? mass[i] + "(" + costs[i] + ") " : "");
+
         }
     }
 }
